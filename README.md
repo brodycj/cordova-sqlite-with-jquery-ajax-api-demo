a6463a08e3588bcf730fa259b15e0ecb6033b8df
Christopher J. Brody <brodybits@litehelpers.net>
2015-10-21 23:26:03 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sample Cordova sqlite Ajax demo
+
+LICENSE: [CC BY 4.0 (Attribution 4.0 International)](https://creativecommons.org/licenses/by/4.0/)
+
+Demonstrates how to use JQuery AJAX together with the [Cordova-sqlite-storage](https://github.com/litehelpers/Cordova-sqlite-storage) transaction feature.
+
+## Credits
+
+- MAJOR LIFESAFER: http://stackoverflow.com/questions/31506957/ajax-call-on-cordova-ios-securityerror-dom-exception-18
+- http://stackoverflow.com/questions/33240009/jquery-json-cordova-issue
+
---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,38 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-console/www/logger.js",
+        "id": "cordova-plugin-console.logger",
+        "clobbers": [
+            "cordova.logger"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+        "id": "cordova-plugin-console.console",
+        "clobbers": [
+            "console"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.0.1",
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-sqlite-storage": "0.7.11"
+}
+// BOTTOM OF METADATA
+});
